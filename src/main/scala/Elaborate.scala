878f34f1c14976305e8b8beaf32622eae8f01e59
ysyx_22051213-Zhang Yuchi <inchinaxiaofeng@gmail.com>
2025-05-01 20:51:18 +0800

    chore(all): Blessing you.

--- src/main/scala/Elaborate.scala
+++ src/main/scala/Elaborate.scala
@@ -1,3 +1,14 @@
+/*
+** 2025 May 1
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+ */
 import circt.stage._
 import chisel3._
 import chisel3.stage._
--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -1,3 +1,14 @@
+/*
+** 2025 May 1
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+ */
 package config
 
 /** 用於選擇架構寬度。
